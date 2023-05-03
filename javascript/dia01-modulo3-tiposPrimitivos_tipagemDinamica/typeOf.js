b3221e4b7114e4d45ff17165ea3943499c72854e
Thedy Wender <thedywender@hotmail.com>
2023-05-02 22:50:03 -0400

    typeof ok

---
+++ javascript/dia01-modulo3-tiposPrimitivos_tipagemDinamica/typeOf.js
@@ -0,0 +1,22 @@
+/* let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail) */
+
+// Area do perímetro de um retângulo
+
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = (2*base) + (2*height);
+
+ console.log(area)
+ console.log(perimeter);
