cfb4fed0c67654ba88cfe54cde44e120ac47e4f9
Thedywender <thedywender@hotmail.com>
2024-03-11 17:57:23 -0400

    add remove_duplicates

---
+++ Ciencias-da-Computação/section03-estrutura-dados/day02-nos-listas-ecadeadas/exercicio_bonus.py
@@ -0,0 +1,47 @@
+from node import Node
+
+
+class DoublyLinked:
+    def __init__(self):
+        self.head == None
+        self.__length == 0
+        self.tail == len(self) - 1
+
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed
+
+    def insert_last(
+        self, value
+    ):  # complexidade O(1) inserção de um item no final da lista
+        if self.is_empty():
+            return self.insert_first(value)
+
+        new_last_value = Node(value)
+        actual_last_value = self.__get_node_at(len(self) - 1)
+        actual_last_value.next = new_last_value
+        self.__length += 1
+
+    def index_of_duplicate(self, value):
+        position = 1
+        current_value = self.head.next
+        while current_value != self.tail:
+            if current_value.value == value:
+                return position
+            current_value = current_value.next
+            position += 1
+        return -1
+
+    def delete_duplicates(linked_list_content):
+        list_with_unique_elements = DoublyLinked()
+
+        while linked_list_content:
+            value_removed = linked_list_content.remove_first()
+            if list_with_unique_elements.index_of(value_removed.value) == -1:
+                list_with_unique_elements.insert_last(value_removed)
+
+        return list_with_unique_elements
--- Ciencias-da-Computação/section03-estrutura-dados/day02-nos-listas-ecadeadas/linked_list_content.py
+++ Ciencias-da-Computação/section03-estrutura-dados/day02-nos-listas-ecadeadas/linked_list_content.py
@@ -125,6 +125,16 @@ class LinkedList:
 
         return -1
 
+    def delete_duplicates(linked_list):
+        list_with_unique_elements = LinkedList()
+
+        while linked_list:
+            current_node = linked_list.remove_first()
+            if list_with_unique_elements.index_of(current_node.value) == -1:
+                list_with_unique_elements.insert_last(current_node.value)
+
+        return list_with_unique_elements
+
 
 # Para testar, apenas rode o arquivo com: `python3 linked_list_content.py` :)
 if __name__ == "__main__":
